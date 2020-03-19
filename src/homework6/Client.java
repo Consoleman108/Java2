7f9439c0d4bcea639df82f7b65fd7ad59fedc071
Consoleman108 <49117521+Consoleman108@users.noreply.github.com>
2020-03-19 16:30:44 +0300

    Homework 6

---
+++ src/homework6/Client.java
@@ -0,0 +1,7 @@
+package homework6;
+// TODO: Необходимо написать клиента для консольного чата
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/homework6/Server.java
@@ -0,0 +1,9 @@
+package homework6;
+// TODO: Необходимо написать сервер для консольного чата
+public class Server {
+    public static void main(String[] args) {
+
+    }
+
+
+}
