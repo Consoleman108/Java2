84845191a182d53f18c00d9a1a7a863cec2e388f
Consoleman108 <49117521+Consoleman108@users.noreply.github.com>
2020-03-21 13:41:57 +0300

    Homework 7.1

--- NetworkClient/src/ru/geekbrains/java2/client/ClientChat.java
+++ NetworkClient/src/ru/geekbrains/java2/client/ClientChat.java
@@ -1,5 +1,5 @@
 package ru.geekbrains.java2.client;
-
+// TODO: Необходимо написать модуль клиента для чата
 public class ClientChat {
     public static void main(String[] args) {
 
--- NetworkServer/src/ru/geekbrains/java2/server/ServerApp.java
+++ NetworkServer/src/ru/geekbrains/java2/server/ServerApp.java
@@ -1,5 +1,5 @@
 package ru.geekbrains.java2.server;
-
+// TODO: Необходимо написать модуль сервера для чата
 public class ServerApp {
     public static void main(String[] args) {
 
